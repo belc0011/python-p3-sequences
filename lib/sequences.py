364b7204f7253f28b303c0a3f251b74a3a37fe96
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-21 12:58:21 -0700

    complete sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length == 0:
+        print(fibonacci_sequence)
+    else:
+        for i in range(length):
+            if i == 0:
+                fibonacci_sequence.append(0)
+            elif i == 1:
+                fibonacci_sequence.append(1)
+            else:
+                fibonacci_sequence.append(fibonacci_sequence[i - 2] + fibonacci_sequence[i - 1])
+        print(fibonacci_sequence)
